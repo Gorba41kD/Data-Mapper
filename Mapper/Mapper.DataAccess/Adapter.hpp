faed470eef42acd3ea3498652d81b61e782bb0bc
ivanboika <deadman2002xxx@gmail.com>
2023-03-13 19:15:45 +0300

    Changed Adapter to see pqxx

--- Mapper/Mapper.DataAccess/Adapter.hpp
+++ Mapper/Mapper.DataAccess/Adapter.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <string>
 #include <pqxx/pqxx>
-#include "AbstractModel.hpp"
+#include "Models.hpp"
 
 namespace Arch
 {
@@ -11,28 +11,12 @@ namespace Arch
 		pqxx::connection _connection;
 		pqxx::nontransaction _nonTran;
 
-
 	public:
 		// host=localhost port=5432 dbname=program user=postgres password =1221909128 <- connectString
 		explicit Adapter(const std::string& connectString = "host=localhost port=5432 dbname=program user=postgres password=1221909128") :_connection(connectString), _nonTran(_connection) {}
 
-		void read(AbstractModel& model, const std::string& groupBy = "")
-		{
-			std::string _sql;
-
-			_sql = "SELECT * FROM ";
-			_sql += '"';
-			_sql += model.getModelName();
-			_sql += '"';
-			_sql += groupBy;
-
-			auto response = _nonTran.exec(_sql.c_str());
-
-			for (auto&& iter = response.begin(); iter != response.end(); ++iter)
-			{
-				model.fillMap(iter);
-			}
-		}
+		void read(AbstractModel& model, const std::string& groupBy = "");
+		
 	};
 }
 
